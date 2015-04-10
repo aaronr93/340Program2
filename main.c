a64f7feea5838830a28caaa4d9d3029a66749614
Zach Nafziger <zachary@nafziger.net>
2015-04-09 21:45:39 -0400

    compiles. beginning of file input

--- main.c
+++ main.c
@@ -2,61 +2,61 @@
 
 
 int main(int argc, char** argv) {
-    do {
+    /*do {*/
 
-        FILE *ifp, *ofp;
-        char *mode = "r";
-        char output_filename[] = "out.txt";
+  //      FILE *ifp, *ofp;
+  //      char *mode = "r";
+  //      char output_filename[] = "out.txt";
 
-        ifp = fopen("in.txt", mode);
+  //      ifp = fopen("in.txt", mode);
 
-        if (ifp == NULL) {
-            fprintf(stderr, "Can't open input file.\n");
-            exit(1);
-        }
+  //      if (ifp == NULL) {
+  //          fprintf(stderr, "Can't open input file.\n");
+  //          exit(1);
+  //      }
 
-<<<<<<< HEAD
-        char* pol;
-        char eof;
-        int qua, cor, id, arr, dur;
-        int num_processes = 0;
-        Process* coll[500];
-        int i = 0;
+  //      char* pol;
+  //      char eof;
+  //      int qua, cor, id, arr, dur;
+  //      int num_processes = 0;
+  //      Process* coll[500];
+  //      int i = 0;
 
-        while (fscanf(ifp, "%c", eof) != EOF) {
+  //      while (fscanf(ifp, "%c", eof) != EOF) {
 
-            if (fscanf(ifp, "fcfs_single", pol) == 1 ||
-                fscanf(ifp, "fcfs_percore",pol) == 1 ||
-                fscanf(ifp, "rr_load",     pol) == 1 ||
-                fscanf(ifp, "rr_percore",  pol) == 1) {
-                // ^ gets the Policy
-                fscanf(ifp, "%d %d", &qua, &pol);   // Get quantum and cores
-            }
+  //          if (fscanf(ifp, "fcfs_single", pol) == 1 ||
+  //              fscanf(ifp, "fcfs_percore",pol) == 1 ||
+  //              fscanf(ifp, "rr_load",     pol) == 1 ||
+  //              fscanf(ifp, "rr_percore",  pol) == 1) {
+  //              // ^ gets the Policy
+  //              fscanf(ifp, "%d %d", &qua, &pol);   // Get quantum and cores
+  //          }
 
-            while (fscanf(ifp, "%d %d %d", &id, &arr, &dur) == 3) {
-                coll[i++] = process_constructor(pol, qua, cor, id, arr, dur);
-                num_processes++;
-            }
-        }
+  //          while (fscanf(ifp, "%d %d %d", &id, &arr, &dur) == 3) {
+  //              coll[i++] = process_constructor(pol, qua, cor, id, arr, dur);
+  //              num_processes++;
+  //          }
+  //      }
 
-        for (i = 0; i < num_processes; i++) {
-            if (strcmp(coll[i]->policy, "FCFS_SINGLE") == true) {
-                fcfs_single(&coll[i]);
-            } else if (strcmp(coll[i]->policy, "FCFS_PERCORE") == true) {
+  //      for (i = 0; i < num_processes; i++) {
+  //          if (strcmp(coll[i]->policy, "FCFS_SINGLE") == true) {
+  //              fcfs_single(&coll[i]);
+  //          } else if (strcmp(coll[i]->policy, "FCFS_PERCORE") == true) {
 
-            } else if (strcmp(coll[i]->policy, "RR_LOAD") == true) {
+  //          } else if (strcmp(coll[i]->policy, "RR_LOAD") == true) {
 
-            } else if (strcmp(coll[i]->policy, "RR_PERCORE") == true) {
+  //          } else if (strcmp(coll[i]->policy, "RR_PERCORE") == true) {
 
-            }
-        }
+  //          }
+  //      }
 
-        ofp = fopen(output_filename, "w");
+  //      ofp = fopen(output_filename, "w");
 
-        if (ofp == NULL) {
-            fprintf(stderr, "Can't open output file.\n");
-            exit(1);
-=======
+  //      if (ofp == NULL) {
+  //          fprintf(stderr, "Can't open output file.\n");
+  //          exit(1);
+		//}
+
     do {
 		printf("How many cores: ");
 		scanf("%d", &numCores);
@@ -88,15 +88,15 @@ int main(int argc, char** argv) {
         } else if (input == 3) {
             // rr_percore
             // Call apropros function
+			rr_percore();
         } else if (input == 4) {
             // rr_load
             rr_load();
         } else {
             printf("Invalid choice.\n");
->>>>>>> 9c8ad9fa8eb6ec19834aec1143bbc3775da975a2
         }
 
-    } while (1);
+    } while(1);
 
     return 0;
 }
