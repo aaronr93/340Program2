d8ea96f5d286dfa69de56407ea9f88f88e3e58f6
Zach Nafziger <zachary@nafziger.net>
2015-04-10 18:41:15 -0400

    ugh..

--- _rr_load.c
+++ _rr_load.c
@@ -14,10 +14,11 @@
 #define _RR_LOAD_
 
 void rr_load(){
+	
 	int i;
-	Process rr_load_p[100];
+	Process rr_load_p[500];
 	Process * rr_load_cores[20][50]; //up to 20 cores with up to 50 processes each
-	int bursts[100];
+	int bursts[500];
 
 	//make the entire array null
 	for(i = 0; i < 20; i++){
@@ -27,6 +28,17 @@ void rr_load(){
 		}
 	}
 
+	
+	
+	// ask user how many processes
+	//printf("How many processes: ");
+
+	// Receive user's choice
+	/*int num_processes, id, arrive, duration, quantum_rr_percore;
+	scanf("%d", &num_processes);*/
+
+	/*printf("What is the quantum: ");
+	scanf("%d", &quantum_rr_percore);*/
 	int quantum_rr_load = quantum;
 	if(quantum < 1){
 		printf("Error. Invalid quantum size.\n");
@@ -34,20 +46,29 @@ void rr_load(){
 	}
 	//adding to a global list of processes
 	for (i = 0; i < num_processes; i++) {//ask user for info manually for now, will need to do file input at some point
+		/*printf("Process %d id: ", i);
+		scanf("%d", &id);*/
+		
 		rr_load_p[i].id = coll[i]->id;
+		/*printf("Process %d arrive time: ", i);
+		scanf("%d", &arrive);*/
 		rr_load_p[i].arrive = coll[i]->arrive;
+		/*printf("Process %d duration: ", i);
+		scanf("%d", &duration);*/
 		rr_load_p[i].duration = coll[i]->duration;
 		rr_load_p[i].done = false;
 		rr_load_p[i].start = -1;
 		rr_load_p[i].running = false;
 		bursts[i] = coll[i]->duration;
-	}
+		
 
+	}
+	
 	//now distribute the processes to the cores, evenly at first since the empty cores will automatically have the lightest load
 
-	for(i = 0; i < numCores; i++){
+	for(i = 0; i <numCores; i++){
 		rr_load_cores[i][0] = &rr_load_p[i];
-		printf("Added process %d to core %d\n", rr_load_cores[i][0]->id, i);
+		//printf("Added process %d to core %d\n", rr_load_cores[i][0]->id, i);
 	}
 	//now do the rest
 	for(i = numCores; i <num_processes; i++){
@@ -61,7 +82,7 @@ void rr_load(){
 			int k = 0, current_load = 0;
 			while(true){
 				if(rr_load_cores[j][k] == NULL){
-					printf("found null spot at Core %d process %d\n", j, k);
+					//printf("found null spot at Core %d process %d\n", j, k);
 					break;
 				}
 				else{
@@ -81,7 +102,7 @@ void rr_load(){
 			 else k++;
 		}
 		rr_load_cores[lightestLoadId][k] = &rr_load_p[i];
-		printf("k = %d. Added process %d to core %d\n", k, rr_load_cores[lightestLoadId][k]->id, lightestLoadId);
+		//printf("k = %d. Added process %d to core %d\n", k, rr_load_cores[lightestLoadId][k]->id, lightestLoadId);
 	}
 
 	//now that all of the processes have been added, run the simulation
@@ -95,17 +116,17 @@ void rr_load(){
 		still_running = false;
 		for(i = 0; i <numCores; i++)
 		{
-
+			
 			int temp = ticker[i];
 			int tempq = quantum_rr_load;
 			while(true)
 			{
-
+				
 
 				if(rr_load_cores[i][ticker[i]] != NULL && !rr_load_cores[i][ticker[i]]->done && rr_load_cores[i][ticker[i]]->arrive <= current_time)//if there is a process at the current ticker position
 				{
 					if(rr_load_cores[i][ticker[i]]->start == -1){
-						printf("process %d has started executing\n", rr_load_cores[i][ticker[i]]->id);
+						//printf("process %d has started executing\n", rr_load_cores[i][ticker[i]]->id);
 						rr_load_cores[i][ticker[i]]->start = current_time;
 					}
 					still_running = true;
@@ -113,32 +134,32 @@ void rr_load(){
 					for(k = 0; k < tempq; k++){
 						rr_load_cores[i][ticker[i]]->duration -= 1;
 						tempq -=1;
-						printf(" process %d tick. tempq = %d remaining duration of process is %d\n.", rr_load_cores[i][ticker[i]]->id, tempq, rr_load_cores[i][ticker[i]]->duration);
+						//printf(" process %d tick. tempq = %d remaining duration of process is %d\n.", rr_load_cores[i][ticker[i]]->id, tempq, rr_load_cores[i][ticker[i]]->duration);
 						if(tempq <= 0) k=tempq;
 					}
 					if(rr_load_cores[i][ticker[i]]->duration <= 0){
 						rr_load_cores[i][ticker[i]]->done = true;
 						rr_load_cores[i][ticker[i]]->finish = current_time + (quantum_rr_load - tempq);
-						printf("Finished executing process %d\n", rr_load_cores[i][ticker[i]]->id);
+						//printf("Finished executing process %d\n", rr_load_cores[i][ticker[i]]->id);
 					}
 				}
-
+				
 				ticker[i]++;
-				printf("ticker %d is now at %d\n", i, ticker[i]);
+				//printf("ticker %d is now at %d\n", i, ticker[i]);
 				if(rr_load_cores[i][ticker[i]] == NULL){
-					printf("The ticker on core %d is at %d so ", i, ticker[i]);
+					//printf("The ticker on core %d is at %d so ", i, ticker[i]);
 					ticker[i] = 0;//reset the ticker if it's gone through each process
-					printf("ticker on core %d has reset\n", i);
+					//printf("ticker on core %d has reset\n", i);
 				}
 				//if you come around to temp again there's nothing left on this core
 				if(ticker[i] == temp){
-					printf("Core %d is done\n", i);
+					//printf("Core %d is done\n", i);
 					break;
 				}
 
 				if(tempq <= 0) break;
 			}
-
+			
 		}
 		current_time += quantum_rr_load;
 	}
--- _rr_percore.c
+++ _rr_percore.c
@@ -16,9 +16,9 @@
 void rr_percore() {
 	srandom((unsigned)time(NULL));
 	int i;
-	Process rr_percore_p[100];
+	Process rr_percore_p[500];
 	Process * rr_percore_cores[20][50]; //up to 20 cores with up to 50 processes each
-	int bursts[100];
+	int bursts[500];
 
 	//make the entire array null
 	for (i = 0; i < 20; i++) {
@@ -28,6 +28,15 @@ void rr_percore() {
 		}
 	}
 
+	// ask user how many processes
+	//printf("How many processes: ");
+
+	// Receive user's choice
+	/*int num_processes, id, arrive, duration, quantum_rr_percore;
+	scanf("%d", &num_processes);*/
+
+	/*printf("What is the quantum: ");
+	scanf("%d", &quantum_rr_percore);*/
 	int quantum_rr_percore = quantum;
 	if(quantum < 1){
 		printf("Error. Invalid quantum size.\n");
@@ -36,14 +45,25 @@ void rr_percore() {
 
 	//adding to a global list of processes
 	for (i = 0; i < num_processes; i++) {//ask user for info manually for now, will need to do file input at some point
+		/*printf("Process %d id: ", i);
+		scanf("%d", &id);*/
 		rr_percore_p[i].id = coll[i]->id;
+		/*printf("Process %d arrive time: ", i);
+		scanf("%d", &arrive);*/
 		rr_percore_p[i].arrive = coll[i]->arrive;
+		/*printf("Process %d duration: ", i);
+		scanf("%d", &duration);*/
 		rr_percore_p[i].duration = coll[i]->duration;
 		rr_percore_p[i].done = false;
 		rr_percore_p[i].start = -1;
 		rr_percore_p[i].running = false;
 		bursts[i] = coll[i]->duration;
+
 	}
+
+
+
+
 
 	for (i = 0; i < num_processes; i++) {
 		int rand_core = rand() % numCores;
@@ -55,7 +75,7 @@ void rr_percore() {
 			else k++;
 		}
 		rr_percore_cores[rand_core][k] = &rr_percore_p[i];
-		printf("k = %d. Added process %d to core %d\n", k, rr_percore_cores[rand_core][k]->id, rand_core);
+		//printf("k = %d. Added process %d to core %d\n", k, rr_percore_cores[rand_core][k]->id, rand_core);
 	}
 
 	//now that all of the processes have been added, run the simulation
@@ -69,17 +89,17 @@ void rr_percore() {
 		still_running = false;
 		for(i = 0; i <numCores; i++)
 		{
-
+			
 			int temp = ticker[i];
 			int tempq = quantum_rr_percore;
 			while(true)
 			{
-
+				
 
 				if(rr_percore_cores[i][ticker[i]] != NULL && !rr_percore_cores[i][ticker[i]]->done && rr_percore_cores[i][ticker[i]]->arrive <= current_time)//if there is a process at the current ticker position
 				{
 					if(rr_percore_cores[i][ticker[i]]->start == -1){
-						printf("process %d has started executing\n", rr_percore_cores[i][ticker[i]]->id);
+						//printf("process %d has started executing\n", rr_percore_cores[i][ticker[i]]->id);
 						rr_percore_cores[i][ticker[i]]->start = current_time;
 					}
 					still_running = true;
@@ -87,32 +107,32 @@ void rr_percore() {
 					for(k = 0; k < tempq; k++){
 						rr_percore_cores[i][ticker[i]]->duration -= 1;
 						tempq -=1;
-						printf(" process %d tick. tempq = %d remaining duration of process is %d\n.", rr_percore_cores[i][ticker[i]]->id, tempq, rr_percore_cores[i][ticker[i]]->duration);
+						//printf(" process %d tick. tempq = %d remaining duration of process is %d\n.", rr_percore_cores[i][ticker[i]]->id, tempq, rr_percore_cores[i][ticker[i]]->duration);
 						if(tempq <= 0) k=tempq;
 					}
-					if (rr_percore_cores[i][ticker[i]]->duration <= 0){
+					if(rr_percore_cores[i][ticker[i]]->duration <= 0){
 						rr_percore_cores[i][ticker[i]]->done = true;
 						rr_percore_cores[i][ticker[i]]->finish = current_time + (quantum_rr_percore - tempq);
-						printf("Finished executing process %d\n", rr_percore_cores[i][ticker[i]]->id);
+						//printf("Finished executing process %d\n", rr_percore_cores[i][ticker[i]]->id);
 					}
 				}
-
+				
 				ticker[i]++;
-				printf("ticker %d is now at %d\n", i, ticker[i]);
+				//printf("ticker %d is now at %d\n", i, ticker[i]);
 				if(rr_percore_cores[i][ticker[i]] == NULL){
-					printf("The ticker on core %d is at %d so ", i, ticker[i]);
+					//printf("The ticker on core %d is at %d so ", i, ticker[i]);
 					ticker[i] = 0;//reset the ticker if it's gone through each process
-					printf("ticker on core %d has reset\n", i);
+					//printf("ticker on core %d has reset\n", i);
 				}
 				//if you come around to temp again there's nothing left on this core
 				if(ticker[i] == temp){
-					printf("Core %d is done\n", i);
+					//printf("Core %d is done\n", i);
 					break;
 				}
 
 				if(tempq <= 0) break;
 			}
-
+			
 		}
 		current_time += quantum_rr_percore;
 	}
